1b986df0c5616d6730afdbc19e9f9395f333933b
Hitesh Mishra <hiteshmishra708@gmail.com>
2021-06-26 09:17:03 +0530

    fix

--- core/serializers.py
+++ core/serializers.py
@@ -16,9 +16,10 @@ class CategorySerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source='cat.name')
     supplier_name = serializers.CharField(source='supplied_by.name')
+    search_auto = serializers.CharField(source='name' + ' ' + 'cat.name' + 'supplied_by.name')
 
     class Meta:
         model = Product
         # fields = ('__all__')
         # fields = ('id', 'name', 'desc',)
-        fields = ('id', 'name', 'desc', 'category_name', 'supplier_name')
+        fields = ('id', 'name', 'desc', 'category_name', 'supplier_name', 'search_auto')
